5fafc8ee7b677b8f39aad9e90eb1e62dde27fd35
mits <dmitrybaycharov@gmail.com>
2021-07-07 13:10:22 -0500

    testing

--- db.js
+++ db.js
@@ -2,10 +2,18 @@
 
 const { Client } = require("pg");
 
-let DB_URI = "postgresql://postgres:myPassword@localhost:5432/biztime";
+function DB_URI() {
+  let DB_URI;
+  if (process.env.NODE_ENV == "prod") {
+    DB_URI = "postgresql://postgres:myPassword@localhost:5432/biztime";
+  } else {
+    DB_URI = "postgresql://postgres:myPassword@localhost:5432/biztime_test";
+  }
+  return DB_URI;
+}
 
 let db = new Client({
-  connectionString: DB_URI,
+  connectionString: DB_URI(),
 });
 
 db.connect();
